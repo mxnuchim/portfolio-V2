50e48e2a12501fb0be7fa224db6e3495e2f1fe7b
Manuchimso Oliver <manuchimoliver779@gmail.com>
2022-09-09 15:54:42 +0100

    Update SocialMedia.jsx

--- frontend-react/src/components/SocialMedia.jsx
+++ frontend-react/src/components/SocialMedia.jsx
@@ -1,23 +1,19 @@
 import React from 'react';
 import { BsTwitter, BsGithub, BsLinkedin } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
 
 const SocialMedia = () => (
   <div className="app__social">
-    <a href="https://twitter.com/manuchim_ix" target="_blank" rel="noreferrer">
+    <div>
       <BsTwitter />
-    <a/>
-//     <a href="https://github.com/mxnuchim" target="_blank" rel="noreferrer">
-//       <div>
-//         <BsGithub />
-//       </div>
-//     <a/>
-//     <a href="https://linkedin.com/in/manuchimoliver" target="_blank" rel="noreferrer">
-//       <div>
-//         <BsLinkedin />
-//       </div>
-//     <a/>
+    </div>
+    <div>
+      <BsGithub />
+    </div>
+    <div>
+      <BsLinkedin />
+    </div>
   </div>
 );
 
 export default SocialMedia;
+
