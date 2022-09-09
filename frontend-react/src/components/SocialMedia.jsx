645e0b0b232f4231d4b1c2f36bcb32482bc0b64f
Manuchimso Oliver <manuchimoliver779@gmail.com>
2022-09-09 15:41:04 +0100

    Update SocialMedia.jsx

--- frontend-react/src/components/SocialMedia.jsx
+++ frontend-react/src/components/SocialMedia.jsx
@@ -4,21 +4,21 @@ import { FaFacebookF } from 'react-icons/fa';
 
 const SocialMedia = () => (
   <div className="app__social">
-  <a href="https://twitter.com/manuchim_ix" target="_blank" rel="noreferrer">
-    <div classname="app_social_single">
-      <BsTwitter />
-    </div>
-  <a/>
-  <a href="https://github.com/mxnuchim" target="_blank" rel="noreferrer">
-    <div classname="app_social_single">
-      <BsGithub />
-    </div>
-  <a/>
-  <a href="https://linkedin.com/in/manuchimoliver" target="_blank" rel="noreferrer">
-    <div classname="app_social_single">
-      <BsLinkedin />
-    </div>
-  <a/>
+    <a href="https://twitter.com/manuchim_ix" target="_blank" rel="noreferrer">
+      <div classname="app_social_single">
+        <BsTwitter />
+      </div>
+    <a/>
+    <a href="https://github.com/mxnuchim" target="_blank" rel="noreferrer">
+      <div classname="app_social_single">
+        <BsGithub />
+      </div>
+    <a/>
+    <a href="https://linkedin.com/in/manuchimoliver" target="_blank" rel="noreferrer">
+      <div classname="app_social_single">
+        <BsLinkedin />
+      </div>
+    <a/>
   </div>
 );
 
