eb35bc5ac8f063ecbc2d19039ae721931ef05da7
mxnuchim <manuchimoliver779@gmail.com>
2022-10-08 18:26:52 +0100

    added social links

--- frontend-react/src/components/SocialMedia.jsx
+++ frontend-react/src/components/SocialMedia.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import { BsTwitter, BsGithub, BsLinkedin } from 'react-icons/bs';
+import { BsTwitter, BsGithub, BsLinkedin, BsMedium, BsEnvelopeFill } from 'react-icons/bs';
 
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <BsTwitter />
+      <a href='mailto:manuchimoliver779@gmail.com'><BsEnvelopeFill /> </a>
     </div>
     <div>
-      <BsGithub />
+      <a href='https://twitter.com/manuchim_ix'><BsTwitter /> </a>
     </div>
     <div>
-      <BsLinkedin />
+      <a href='https://github.com/mxnuchim'><BsGithub /> </a>
+    </div>
+    <div>
+      <a href='https://www.linkedin.com/in/manuchimoliver'><BsLinkedin /> </a>
+    </div>
+    <div>
+      <a href='https://www.medium.com/@manuchimoliver779'><BsMedium /> </a>
     </div>
   </div>
 );
