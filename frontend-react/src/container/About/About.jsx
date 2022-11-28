641f27981f7a97f9223f6d658e95733322ac49db
mxnuchim <manuchimoliver779@gmail.com>
2022-11-28 11:52:08 +0100

    commmit

--- frontend-react/src/container/About/About.jsx
+++ frontend-react/src/container/About/About.jsx
@@ -32,7 +32,7 @@ const About = () => {
         <span>TypeScript</span>. my core strengths are Frontend development{" "}
         <span>(React/React Native)</span>, Backend Development{" "}
         <span>(Node.js)</span>, cloud computing services <span>(AWS)</span> and
-        I'm also learning IOS development <span>(SwiftUI)</span> <br />
+        I'm also learning IOS development with <span>(SwiftUI)</span> <br />
         I'm quietly confident, naturally curious, and constantly working on
         improving my skills, one solved problem at a time.
       </h2>
