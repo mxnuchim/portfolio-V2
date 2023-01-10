4902b6dd4adda1cd2097aba42888d368ee49bdcf
Manuchimso Oliver <manuchimoliver779@gmail.com>
2023-01-10 18:53:57 +0100

    Update Header.jsx

--- frontend-react/src/container/Header/Header.jsx
+++ frontend-react/src/container/Header/Header.jsx
@@ -28,7 +28,7 @@ const Header = () => {
           <a
             target="_blank"
             rel="noreferrer"
-            href="https://drive.google.com/file/d/12HKmHk-vZQZE2dsbVy7BAqv82k6mfq_l/view?usp=sharing"
+            href="https://drive.google.com/file/d/1zVJghA5wNqs6EezMalBKHpfoM3yN4P5k/view?usp=sharing"
           >
             <button type="button" className="p-text1">
               Résumé/CV
